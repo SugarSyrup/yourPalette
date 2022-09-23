143d877a1356e3f4179a17c93c51c72438b1066e
JaYun <tlfvm04@naver.com>
2022-09-23 23:40:08 +0900

    COLOR 재정의

--- static/module/TONE.js
+++ static/module/TONE.js
@@ -1,28 +1,40 @@
 export const TONE_DICT = {};
 
-TONE_DICT["whitish_saturation"] = 40;
-TONE_DICT["whitish_lightness"] = 20;
+TONE_DICT["very_pale_saturation"] = 20;
+TONE_DICT["very_pale_lightness"] = 90;
+TONE_DICT["pale_grayish_saturation"] = 20;
+TONE_DICT["pale_grayish_lightness"] = 70;
+TONE_DICT["middle_grayish_saturation"] = 20;
+TONE_DICT["middle_grayish_lightness"] = 50;
+TONE_DICT["very_grayish_saturation"] = 20;
+TONE_DICT["very_grayish_lightness"] = 30;
+TONE_DICT["very_dark_saturation"] = 20;
+TONE_DICT["very_dark_lightness"] = 10;
+
+TONE_DICT["pale_saturation"] = 40;
+TONE_DICT["pale_lightness"] = 72;
 TONE_DICT["light_grayish_saturation"] = 40;
-TONE_DICT["light_grayish_lightness"] = 35;
+TONE_DICT["light_grayish_lightness"] = 57;
 TONE_DICT["grayish_saturation"] = 40;
-TONE_DICT["grayish_lightness"] = 50;
+TONE_DICT["grayish_lightness"] = 42;
 TONE_DICT["dark_grayish_saturation"] = 40;
-TONE_DICT["dark_grayish_lightness"] = 65;
-TONE_DICT["blackish_saturation"] = 40;
-TONE_DICT["blackish_lightness"] = 80;
-TONE_DICT["pale_saturation"] = 60;
-TONE_DICT["pale_lightness"] = 27;
+TONE_DICT["dark_grayish_lightness"] = 27;
+
+TONE_DICT["light_saturation"] = 60;
+TONE_DICT["light_lightness"] = 72;
 TONE_DICT["soft_saturation"] = 60;
-TONE_DICT["soft_lightness"] = 42;
+TONE_DICT["soft_lightness"] = 57;
 TONE_DICT["dull_saturation"] = 60;
-TONE_DICT["dull_lightness"] = 57;
+TONE_DICT["dull_lightness"] = 42;
 TONE_DICT["dark_saturation"] = 60;
-TONE_DICT["dark_lightness"] = 72;
-TONE_DICT["light_saturation"] = 80;
-TONE_DICT["light_lightness"] = 35;
+TONE_DICT["dark_lightness"] = 27;
+
+TONE_DICT["bright_saturation"] = 80;
+TONE_DICT["bright_lightness"] = 70;
 TONE_DICT["strong_saturation"] = 80;
 TONE_DICT["strong_lightness"] = 50;
 TONE_DICT["deep_saturation"] = 80;
-TONE_DICT["deep_lightness"] = 65;
+TONE_DICT["deep_lightness"] = 30;
+
 TONE_DICT["vivid_saturation"] = 100;
 TONE_DICT["vivid_lightness"] = 50;
